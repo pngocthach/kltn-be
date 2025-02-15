db5b8a162effd91d4bd27c254127882d3a9f6644
jelly <jellypnt1311@gmail.com>
2025-02-16 02:49:43 +0700

    feat: update TypeScript configuration and add tsup for module bundling

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  format: ["esm"],
+  target: "node18",
+  noExternal: ["fs", "path", "os"], // Giữ nguyên các module Node.js
+  platform: "node",
+  shims: true, // Thêm shims cho các module không tương thích
+  esbuildOptions(options) {
+    options.banner = {
+      js: 'import { createRequire } from "module"; const require = createRequire(import.meta.url);',
+    };
+  },
+});
